38d1554748352464a312edd2665cb363f770230d
Viktor <victor.sochi.live@gmail.com>
2026-01-03 20:52:09 +0300

    remove unused platformio.ini target, move to adafruit neopixel lib, O2 optimisation

--- src/esp32/aLED_esp32.cpp
+++ src/esp32/aLED_esp32.cpp
@@ -1,25 +1,35 @@
-#include <FastLED.h>
 #include <Arduino.h>
 #include <projectDefs.h>
+
+#include <Adafruit_NeoPixel.h>
 
 const int NUM_LEDS = 2;
 
 
-CRGB leds[NUM_LEDS];
+Adafruit_NeoPixel strip(2, leds_pin, NEO_RGB + NEO_KHZ800);
+// Argument 1 = Number of pixels in NeoPixel strip
+// Argument 2 = Arduino pin number (most are valid)
+// Argument 3 = Pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 
 void led_init() {
     pinMode(leds_pin, OUTPUT);
-    FastLED.addLeds<WS2812, leds_pin, RGB>(leds, 2);
+    strip.begin();
+    strip.show();
 }
 
 void led1_rgb(uint8_t red_col, uint8_t green_col, uint8_t blue_col){
-    leds[1] = CRGB(red_col, green_col, blue_col);
+    strip.setPixelColor(1, red_col, green_col, blue_col);
 }
 
 void led2_rgb(uint8_t red_col, uint8_t green_col, uint8_t blue_col){
-    leds[0] = CRGB(red_col, green_col, blue_col);
+    strip.setPixelColor(0, red_col, green_col, blue_col);
 }
 
 void led_update(){
-    FastLED.show();
+    strip.show();
 }
--- src/esp32/aLED_esp32.h
+++ src/esp32/aLED_esp32.h
@@ -1,5 +1,3 @@
-
-#include <FastLED.h>
 #include <Arduino.h>
 
 
